d37719b148628afd8dc3af08baa6ce3a2a7cc359
DungPham999 <mydung1.qtnh@gmail.com>
2026-09-25 00:25:20 +0700

    Update config.js

--- ets2024-test2/config.js
+++ ets2024-test2/config.js
@@ -1,8 +1,8 @@
 window.TEST_CONFIG = {
   year: 2024,
-  testNumber: 3,
-  title: "ETS 2024 · Test 3",
-  storageKey: "ets2024-t3",
+  testNumber: 2,
+  title: "ETS 2024 · Test 2",
+  storageKey: "ets2024-t2",
   homeUrl: "../",
   readingEnabled: false,
   readingUrl: "./reading/"
