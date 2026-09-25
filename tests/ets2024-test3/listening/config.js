9b51814c6b082fbc9dd32c9a67299260d7435b31
DungPham999 <mydung1.qtnh@gmail.com>
2026-09-25 18:19:15 +0700

    Delete tests/ets2024-test3/listening directory

--- tests/ets2024-test3/listening/config.js
+++
@@ -1,9 +0,0 @@
-window.TEST_CONFIG = {
-  year: 2024,
-  testNumber: 3,
-  title: "ETS 2024 · Test 3",
-  storageKey: "ets2024-t3",
-  homeUrl: "../../",
-  readingEnabled: false,
-  readingUrl: "../../shared/reading/reading.html?test=ets2024-t3"
-};
