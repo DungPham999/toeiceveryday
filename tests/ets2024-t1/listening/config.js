715570cc4861c299aa2ed2076867dc2e504e6192
DungPham999 <mydung1.qtnh@gmail.com>
2026-09-25 12:51:20 +0700

    upload file rieng Lis T1

---
+++ tests/ets2024-t1/listening/config.js
@@ -0,0 +1,9 @@
+window.TEST_CONFIG = {
+  year: 2024,
+  testNumber: 1,
+  title: "ETS 2024 · Test 1",
+  storageKey: "ets2024-t1",
+  homeUrl: "../",
+  readingEnabled: false,
+  readingUrl: "./reading/"
+};
