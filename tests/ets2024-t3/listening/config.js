64295c103205ed998e23429e8ddc91b592cd4732
DungPham999 <mydung1.qtnh@gmail.com>
2026-09-25 18:29:02 +0700

    Add files via upload

---
+++ tests/ets2024-t3/listening/config.js
@@ -0,0 +1,9 @@
+window.TEST_CONFIG = {
+  year: 2024,
+  testNumber: 3,
+  title: "ETS 2024 · Test 3",
+  storageKey: "ets2024-t3",
+  homeUrl: "../../",
+  readingEnabled: false,
+  readingUrl: "../../shared/reading/reading.html?test=ets2024-t3"
+};
